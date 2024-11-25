d4e15c9ac2a7ae0cfac0ff9f8273804b8be456c8
unknown <imraan.mohamedhoesein@student.rocva.nl>
2024-11-25 12:19:45 +0100

    planeten functie

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/planets', function () {
+    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+});
