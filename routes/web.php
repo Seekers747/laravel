36bf6f5c871753acedc70e9c468fbc2608845f32
unknown <imraan.mohamedhoesein@student.rocva.nl>
2024-11-29 10:34:05 +0100

    opdracht: views. af

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Opdracht: de eerste route
+
+// Route::get('/planets', function () {
+//     return (["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"]);
+// });
+
+// Opdracht: Views
+
 Route::get('/planets', function () {
-    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets', ['planets' => $planets]);
 });
